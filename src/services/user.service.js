e4acc5549405db319b71fa01568c76419263da30
Gabriel <soramoretto@gmail.com>
2022-06-23 19:51:38 -0300

    Create user.service.js

---
+++ src/services/user.service.js
@@ -0,0 +1,11 @@
+export default function userValidation(username, email, password, passwordConfirm, res){
+    if(!username || typeof username !== 'string'){
+        res.status(422).json({status: 'error', error: 'Invalid username'})
+    }
+    if(!email || typeof email !== 'string' || !email.match(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,4}$/)){
+        res.status(422).json({status: 'error', error: 'Invalid email format'})
+    }
+    if(password.length < 6 || typeof password !== 'string' || password !== passwordConfirm){
+        res.status(422).json({status: 'error', error: 'Password too short or confirmation doesnt match'})
+    }
+}
