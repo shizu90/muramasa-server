3b95b97afc87fb3c340c8eac16e923a4016d54e5
Gabriel <soramoretto@gmail.com>
2022-06-24 16:48:26 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -17,14 +17,117 @@ export default function appRoutes(app){
         res.status(200).json({user})
     })
 
+    app.get('/api/user/:id/animelist', async(req, res) => {
+        const id = req.params.id
+        const user = await UserModel.findById(id, '-password')
+        if(!user){
+            return res.status(404).json({status: 'error', error: 'User not found'})
+        }
+
+        res.status(200).json({user: user.animeList})
+    })
+
+    app.get('/api/user/:id/mangalist', async(req, res) => {
+        const id = req.params.id
+        const user = await UserModel.findById(id, '-password')
+        if(!user){
+            return res.status(404).json({status: 'error', error: 'User not found'})
+        }
+        res.status(200).json({user: user.mangaList})
+    })
+
     app.post('/api/user/register', async (req, res) => {   
         try {
             const user = await createUser(req, res)
-            console.log(`User created successfully: ${user._id}`)
+            console.log(`User created successfully: ${user}`)
             res.json({body: req.body})
         }catch(e){
             res.json({status: 'error', message: `${e.message}`})
         }
+    })
+
+    app.put('/api/user/:id/animelist/:anime/:status', async (req, res) => {
+        const id = req.params.id
+        const anime = req.params.anime
+        const status = req.params.status
+        if(anime.id){
+            if(status === 'watching'){
+                const user = await UserModel.findOneAndUpdate({_id: id}, {$addToSet: {'animeList.watching': anime}})
+                if(!user){
+                    return res.status(404).json({status: 'error', error: 'User not found'})
+                }
+                res.status(200).json({status: 'success', success: user.animeList})
+            }
+            else if(status === 'completed'){
+                const user = await UserModel.findOneAndUpdate({_id: id}, {$addToSet: {'animeList.completed': anime}})
+                if(!user){
+                    return res.status(404).json({status: 'error', error: 'User not found'})
+                }
+                res.status(200).json({status: 'success', success: user.animeList}) 
+            }
+            else if(status === 'dropped'){
+                const user = await UserModel.findOneAndUpdate({_id: id}, {$addToSet: {'animeList.dropped': anime}})
+                if(!user){
+                    return res.status(404).json({status: 'error', error: 'User not found'})
+                }
+                res.status(200).json({status: 'success', success: user.animeList})
+            }
+            else if(status === 'plans'){
+                const user = await UserModel.findOneAndUpdate({_id: id}, {$addToSet: {'animeList.plans': anime}})
+                if(!user){
+                    return res.status(404).json({status: 'error', error: 'User not found'})
+                }
+                res.status(200).json({status: 'success', success: user.animeList})    
+            }else{
+                res.status(404).json({status: 'error', error: 'Status not found'})
+            }
+        }else{
+            res.status(400).json({status: 'error', error: 'Invalid param type'})
+        }
+    })
+
+    app.put('/api/user/:id/mangalist/:manga/:status', async (req, res) => {
+        const id = req.params.id
+        const manga = req.params.manga
+        const status = req.params.status
+        if(manga.id){
+            if(status === 'reading'){
+                const user = await UserModel.findOneAndUpdate({_id: id}, {$addToSet: {'mangaList.reading': manga}})
+                if(!user){
+                    return res.status(404).json({status: 'error', error: 'User not found'})
+                }
+                res.status(200).json({status: 'success', success: user.mangaList})
+            }
+            else if(status === 'completed'){
+                const user = await UserModel.findOneAndUpdate({_id: id}, {$addToSet: {'mangaList.completed': manga}})
+                if(!user){
+                    return res.status(404).json({status: 'error', error: 'User not found'})
+                }
+                res.status(200).json({status: 'success', success: user.mangaList}) 
+            }
+            else if(status === 'dropped'){
+                const user = await UserModel.findOneAndUpdate({_id: id}, {$addToSet: {'mangaList.dropped': manga}})
+                if(!user){
+                    return res.status(404).json({status: 'error', error: 'User not found'})
+                }
+                res.status(200).json({status: 'success', success: user.mangaList})
+            }
+            else if(status === 'plans'){
+                const user = await UserModel.findOneAndUpdate({_id: id}, {$addToSet: {'mangaList.plans': manga}})
+                if(!user){
+                    return res.status(404).json({status: 'error', error: 'User not found'})
+                }
+                res.status(200).json({status: 'success', success: user.mangaList})    
+            }else{
+                res.status(404).json({status: 'error', error: 'Status not found'})
+            }
+        }else{
+            res.status(400).json({status: 'error', error: 'Invalid param type'})
+        }
+    })
+
+    app.put('/api/user/:id/animelist/:anime/:status/delete', async (req, res) => {
+        
     })
 
     app.post('/api/user/login', async (req, res) => {
@@ -55,7 +158,7 @@ export default function appRoutes(app){
                 id: user._id,
             }, secret)
 
-            res.status(200).json({status: 'success', success: 'Succesfully auth fulfillment', token})
+            res.status(200).json({status: 'success', success: 'Succesfully auth fulfillment', token, user: user._id})
         }catch(e){
             console.log(e)
             res.status(500).json({status: 'error', error: 'Occurred an error on the server'})
