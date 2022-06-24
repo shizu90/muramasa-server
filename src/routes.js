b6e9d3cb787cd5d43d48736c09d777b172c344f3
Gabriel <soramoretto@gmail.com>
2022-06-24 01:39:37 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,20 @@
 import createUser from './controller/user.controller.js'
 import bcrypt from 'bcrypt'
 import UserModel from './model/User.js'
+import jwt from 'jsonwebtoken'
 
 export default function appRoutes(app){
     app.get('/api/health', (req, res) => {
         res.json({status: 'ok'})
     })
     
-    app.get('/api/users', (req, res) => {
-
+    app.get('/api/user/:id', async (req, res) => {
+        const id = req.params.id
+        const user = await UserModel.findById(id, '-password')
+        if(!user){
+            return res.status(404).json({status: 'error', error: 'User not found'})
+        }
+        res.status(200).json({user})
     })
 
     app.post('/api/user/register', async (req, res) => {   
@@ -41,6 +47,18 @@ export default function appRoutes(app){
 
         if(!checkPassword){
             return res.status(422).json({status: 'error', error: 'Invalid password'})
+        }
+
+        try{
+            const secret = process.env.SECRET
+            const token = jwt.sign({
+                id: user._id,
+            }, secret)
+
+            res.status(200).json({status: 'success', success: 'Succesfully auth fulfillment', token})
+        }catch(e){
+            console.log(e)
+            res.status(500).json({status: 'error', error: 'Occurred an error on the server'})
         }
     })
 
