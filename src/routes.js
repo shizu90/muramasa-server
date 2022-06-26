1042c57a8a537231f497e20986d9a50191f84987
Gabriel <soramoretto@gmail.com>
2022-06-26 20:32:06 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import createUser from './controller/user.controller.js'
 import logger from './utils/logger.js'
 import UserModel from './model/User.js'
 import jwt from 'jsonwebtoken'
+import parseJwt from './utils/parseJWT.js'
 import { manageAnimeList, manageMangaList } from './services/list.service.js'
 import sessionValidation from './services/session.service.js'
 
@@ -15,8 +16,9 @@ export default function appRoutes(app){
         res.status(200).json({users})
     })
 
-    app.get('/api/user/:id', async (req, res) => {
-        const id = req.params.id
+    app.get('/api/user/:token', async (req, res) => {
+        const token = req.params.token
+        const id = parseJwt(token)
         const user = await UserModel.findById(id, '-password')
         if(!user){
             return res.status(404).json({status: 'error', error: 'User not found'})
@@ -24,8 +26,9 @@ export default function appRoutes(app){
         res.status(200).json({user})
     })
 
-    app.get('/api/user/:id/animelist', async(req, res) => {
-        const id = req.params.id
+    app.get('/api/user/:token/animelist', async(req, res) => {
+        const token = req.params.token
+        const id = parseJwt(token)
         const user = await UserModel.findById(id, '-password')
         if(!user){
             return res.status(404).json({status: 'error', error: 'User not found'})
@@ -34,8 +37,9 @@ export default function appRoutes(app){
         res.status(200).json({user: user.animeList})
     })
 
-    app.get('/api/user/:id/mangalist', async(req, res) => {
-        const id = req.params.id
+    app.get('/api/user/:token/mangalist', async(req, res) => {
+        const token = req.params.token
+        const id = parseJwt(token)
         const user = await UserModel.findById(id, '-password')
         if(!user){
             return res.status(404).json({status: 'error', error: 'User not found'})
@@ -43,8 +47,9 @@ export default function appRoutes(app){
         res.status(200).json({user: user.mangaList})
     })
 
-    app.get('/api/user/:id/favorites', async(req, res) => {
-        const id = req.params.id
+    app.get('/api/user/:token/favorites', async(req, res) => {
+        const token = req.params.token
+        const id = parseJwt(token)
         const user = await UserModel.findById(id, '-password')
         if(!user){
             return res.status(404).json({stauts: 'error', error: 'User not found'})
@@ -69,30 +74,37 @@ export default function appRoutes(app){
 
     //Add or remove media to list
 
-    app.put('/api/user/:id/animelist/:method', async (req, res) => {
-        const id = req.params.id
+    app.put('/api/user/:token/animelist/:method', async (req, res) => {
+        const token = req.params.token
+        const id = parseJwt(token)
         const method = req.params.method
         const {anime, status} = req.body
         manageAnimeList(method, JSON.parse(anime), status, id, res)
     })
 
-    app.put('/api/user/:id/mangalist/:method', async (req, res) => {
-        const id = req.params.id
+    app.put('/api/user/:token/mangalist/:method', async (req, res) => {
+        const token = req.params.token
+        const id = parseJwt(token)
         const method = req.params.method
         const {manga, status} = req.body
         manageMangaList(method, JSON.parse(manga), status, id, res)
     })
 
-    app.put('/api/user/:id/favorites/:method', async(req, res) => {
-        const id = req.params.id
+    app.put('/api/user/:token/favorites/:method', async(req, res) => {
+        const token = req.params.token
+        const id = parseJwt(token)
         const method = req.params.method
         const {media} = req.body
+        const user = await UserModel.findById(id, '-password')
+        if(!user){
+            res.status(404).json({status: 'error', error: 'User not found'})
+        }
         if(method === 'push'){
-            await UserModel.findByIdAndUpdate({_id: id}, {$push: {favorites: media}})
-            res.status(200)
+            await user.updateOne({$addToSet: {favorites: media}})
+            res.status(200).json({status: 'success', success: user.favorites})
         }else{
-            await UserModel.findByIdAndUpdate({_id: id}, {$pull: {favorites: media}})
-            res.status(200)
+            await user.updateOne({$pull: {favorites: media}})
+            res.status(200).json({status: 'success', success: user.favorites})
         }
     })
 
@@ -106,7 +118,7 @@ export default function appRoutes(app){
                 id: user._id,
             }, secret)
 
-            res.status(200).json({status: 'success', success: 'Succesfully auth fulfillment', token, user: user._id})
+            res.status(200).json({status: 'success', success: 'Succesfully auth fulfillment', token})
         }catch(e){
             logger.error(e)
             res.status(500).json({status: 'error', error: 'Occurred an error on the server'})
