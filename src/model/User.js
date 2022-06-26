e7da4da6fba307cf4418754d240561911ae8364f
Gabriel <soramoretto@gmail.com>
2022-06-26 20:32:04 -0300

    Update User.js

--- src/model/User.js
+++ src/model/User.js
@@ -4,9 +4,19 @@ const UserSchema = new mongoose.Schema({
     username: {type: String, required: true, unique: true},
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
-    animeList: {type: Object, required: true},
-    mangaList: {type: Object, required: true},
-    favorites: {type: Array, required: true}
+    animeList: {
+        watching: [],
+        completed: [],
+        dropped: [],
+        plans: [],
+    },
+    mangaList: {
+        reading: [],
+        completed: [],
+        dropped: [],
+        plans: []
+    },
+    favorites: []
 }, { collection: 'users'})
 
 const UserModel = mongoose.model('UserSchema', UserSchema)
