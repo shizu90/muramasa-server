58bea8742f83ef7f2840b0563d0e9de32cac26bf
Gabriel <soramoretto@gmail.com>
2022-06-29 15:12:26 -0300

    Update user.controller.js

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,18 +1,28 @@
 import UserModel from "../model/User.js"
 import bcrypt from 'bcrypt'
-import userValidation from "../services/user.service.js"
 
 export default async function createUser(req, res){
     try {
         const {username, email, password, passwordConfirm} = req.body
         const userExistsByEmail = await UserModel.findOne({email: email})
         const userExistsByUsername = await UserModel.findOne({username: username})
-        userValidation(username, email, password, passwordConfirm, res)
+        if(!email || typeof email !== 'string' || !email.match(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,4}$/)){
+            res.status(422).json({status: 'error', error: 'Invalid email format'})
+            return undefined
+        }
+        if(!username || typeof username !== 'string'){
+            res.status(422).json({status: 'error', error: 'Invalid username'})
+            return undefined
+        }
+        if(password.length < 6 || typeof password !== 'string' || password !== passwordConfirm){
+            res.status(422).json({status: 'error', error: 'Password too short or confirmation doesnt match'})
+            return undefined
+        }
         if(userExistsByEmail){
-            res.status(422).json({status: 'error', error: 'User already exists, please use another email'})
+            res.status(422).json({status: 'error', error: 'Email already in use'})
             return undefined
         }else if(userExistsByUsername){
-            res.status(422).json({status: 'error', error: 'User already exists, please use another username'})
+            res.status(422).json({status: 'error', error: 'Username already taken'})
             return undefined
         }else{
             const user = UserModel.create({
