ec4d1f18b07836c3202db00fbbe5d0ebfc3a7b9b
Gabriel <soramoretto@gmail.com>
2022-06-29 01:50:07 -0300

    Update session.service.js

--- src/services/session.service.js
+++ src/services/session.service.js
@@ -1,17 +1,8 @@
-import bcrypt from 'bcrypt'
-
-export default async function sessionValidation(email, password, response, user){
-    if(!email || user.email !== email){
-        return response.status(422).json({status: 'error', error: 'Invalid email'})
+export default async function sessionValidation(email, password, response){
+    if(!email){
+        response.status(422).json({status: 'error', error: 'Invalid email'})
     }
     if(!password){
-        return response.status(422).json({status: 'error', error: 'Invalid password'})
-    }
-    if(!user){
-        return response.status(404).json({status: 'error', error: 'User not found'})
-    }
-    const checkPassword = await bcrypt.compare(password, user.password)
-    if(!checkPassword){
-        return response.status(422).json({status: 'error', error: 'Invalid password'})
+        response.status(422).json({status: 'error', error: 'Invalid password'})
     }
 }
