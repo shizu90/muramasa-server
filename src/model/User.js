8cd7d704b1c83997113ea5e57f3e4ee4b9925f18
Gabriel <soramoretto@gmail.com>
2022-07-01 01:03:36 -0300

    Update User.js

--- src/model/User.js
+++ src/model/User.js
@@ -2,6 +2,8 @@ import mongoose from "mongoose"
 
 const UserSchema = new mongoose.Schema({
     username: {type: String, required: true, unique: true},
+    propic: {type: String},
+    bio: {type: String},
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
     animeList: {
